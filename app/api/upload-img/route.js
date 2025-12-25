a5c68a63e0f34650d10549ddf0fdd591bab6b248
Anshu Gupta Laptop <anshuguptacell@gmail.com>
2025-12-25 19:27:32 +0530

    created uploadToCloudinary

--- app/api/upload-img/route.js
+++ app/api/upload-img/route.js
@@ -1,23 +1,15 @@
 import cloudinary from "@/lib/cloudinary";
+import uploadToCloudinary from "@/lib/uploadToCloudinary";
 
 export const POST = async (req) => {
     const formData = await req.formData()
-    const {name, age} = JSON.parse(formData.get("data"))
     const imgFile = formData.get("img")
 
-    const buffer = Buffer.from(await imgFile.arrayBuffer())
-    const res = await new Promise((resolve, reject) => {
-        const stream = cloudinary.uploader.upload_stream({ folder: "eCommerce" },
-            (err, res) => (err ? reject(err) : resolve(res))
-        )
-        stream.end(buffer);
-    })
-
+   const url = await uploadToCloudinary(imgFile)
 
     return Response.json({
         success: true,
-        url: res.secure_url,
-        res
+        url
     })
 
 }
